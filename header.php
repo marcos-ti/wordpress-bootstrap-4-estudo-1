ce72d2c788e288aa9ea7781555925ff78c81fd70
Marcos <saoroque@hotmail.com>
2021-08-22 17:16:36 -0300

    Adicionando classes bootstrap

--- header.php
+++ header.php
@@ -2,15 +2,19 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Curso WordPress</title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<p>Este é um parágrafo de referência.</p>
 	<header>
 		<section class="top-bar">
-			<div class="social-media-icons">Ícones Sociais</div>
-			<div class="search">Pesquisa</div>
+			<div class="container">
+				<div class="row">
+					<div class="social-media-icons col-xl-9 col-sm-7 col-6">Ícones Sociais</div>
+					<div class="search col-xl-3 col-sm-5 col-6 text-right">Pesquisa</div>
+				</div>
+			</div>
 		</section>
 		<section class="menu-area">
 			<section class="logo">Logo</section>
